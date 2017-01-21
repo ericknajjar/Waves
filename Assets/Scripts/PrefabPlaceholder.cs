b3230c5b7857c360a1ea0d17018e24dbd5dcf610
ericknajjar <ericknajjar@gmail.com>
2017-01-21 00:40:17 -0200

    prefab placeholder

---
+++ Assets/Scripts/PrefabPlaceholder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabPlaceholder : MonoBehaviour
+{
+
+	[SerializeField]
+	GameObject m_prefab;
+
+	void Awake ()
+	{
+		if (transform.parent != null)
+			GameObject.Instantiate (m_prefab, transform.position, Quaternion.identity, transform.parent);
+		else
+			GameObject.Instantiate (m_prefab, transform.position, Quaternion.identity);
+
+		Destroy (gameObject);
+	}
+
+}
+
